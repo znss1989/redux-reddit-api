06527081a4ef2e004da9c05257ac5381e45f550e
znss1989 <znss1989@gmail.com>
2017-03-24 16:12:29 +0800

    flat structure

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
  var webpack = require('webpack');
      
  module.exports = {
-     entry: './src/main.js',
+     entry: './main.js',
      output: {
          path: path.resolve(__dirname, 'build'),
          filename: 'main.bundle.js'
