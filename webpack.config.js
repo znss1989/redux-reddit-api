f4ccbce282ccb1e83defb876516865c0e9cf5df2
znss1989 <znss1989@gmail.com>
2017-03-24 16:11:14 +0800

    structure

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
  var webpack = require('webpack');
      
  module.exports = {
-     entry: './main.js',
+     entry: './src/main.js',
      output: {
          path: path.resolve(__dirname, 'build'),
          filename: 'main.bundle.js'
